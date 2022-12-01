5db1762829dfbc7e24d9f391b4af450465e1acae
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-01 10:02:57 -0800

    task added

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-print("{a} + {b} = {}".format(add(a, b)))
-print("{a} - {b} = {}".format(sub(a, b)))
-print("{a} / {b} = {}".format(dv(a, b)))
-print("{a} * {b} = {}".format(mul(a, b)))
-
+print(f"{a} + {b} = {add(a, b)}")
+print(f"{a} - {b} = {sub(a, b))}")
+print(f"{a} / {b} = {div(a, b)}")
+print(f"{a} * {b} = {mul(a, b)}")
