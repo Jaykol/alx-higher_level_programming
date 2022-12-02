ab1aa66633a950ee9934203126ed72cafb656951
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-02 01:01:36 -0800

    tasks rewritten

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-n = len(sys.argv)
-print("{} argument:".format(n))
-for x in sys.argv:
-    print("{}: {}".format(n, x))
+if __name__ == "__main__":
+    n = len(sys.argv)
+
+    if n <= 1:
+        print("0 argument.")
+    elif n == 2:
+        print("{:d} argument:".format(n -1))
+    else:
+        print("{:d} arguments:".format(n -1))
+
+    for x in range(1, n):
+        print("{:d}: {}".format(x, sys.argv[x]))
