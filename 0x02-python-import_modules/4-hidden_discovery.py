49c637e637217549a70ed98bf4986b022714c469
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-02 01:36:08 -0800

    print hidden names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,6 @@ import hidden_4
 if __name__ == "__main__":
 
     for name in dir(hidden_4):
-        if name[0:2] != "__":
-            print(name)
+        if name[0:2] == "__":
+            continue
+        print(name)
