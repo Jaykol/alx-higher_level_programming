63c4787d517a879820b6373452dab9d045b518e9
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-05 15:56:02 -0800

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
     if sentence == "":
-        return sentencee[0] = None
+        return sentence[0] = None
     else:
         return len(sentence), sentence[0]
