100c540ca07efbb0524d1437e213fc1c2f02f5a6
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-05 07:35:29 -0800

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in the matrix:
+        for j in the range(len(i)):
+            if j != len(i) - 1:
+                print("{:d}".format(i[j]), end=' ')
+            else:
+                print("{:d}".format(i[j], end='')
+        print()
+
