f92f3da9e595b5813a5de488ba92f4eb81277c81
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-05 16:11:56 -0800

    find multiples of two

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    for n in my_list:
+        if n % 2 == 0:
+            return True
+        else:
+            return False
+    return my_list
