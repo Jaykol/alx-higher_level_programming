8c7aaa8d01006b0300cb08af376e6caeaf35874c
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-15 05:12:09 -0800

    delete key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    return a_dictionary.pop(key)
+    if key in a_dictionary.keys():
+        a_dictionary.pop()
+
+    return a_dictionary
