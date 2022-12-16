3f850a2bae975324f36b03a447c883cd5f9fa268
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-16 13:12:27 -0800

    show highest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    for k in a_dictionary:
-        if k == NULL:
-            return None
-        else:
-            return k
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
