86e1472de21ea3b2704ba1f21c8f493c2e934e5c
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-19 10:42:55 -0800

    corrections

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,8 +8,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print('{:d}'.format(my_list[i]), end='')
             printed += 1
 
-    except:
-        continue
+        except:
+            continue
 
-    print('\n')
+    print()
     return printed
