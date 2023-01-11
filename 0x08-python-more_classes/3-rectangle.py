069e0e9df4a7ea43448c017d6fdc376d7b20306b
jesutofunmi <jekola.ajekola@gmail.com>
2023-01-11 06:14:31 -0800

    new commit

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
